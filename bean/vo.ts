c395fce9f61d9e51e3747947ebcdda98f6c2de20
baixiaoshengtsc <485434766@qq.com>
2023-07-17 17:55:49 +0800

    ✨ feat: 新增login'vo

--- bean/vo.ts
+++ bean/vo.ts
@@ -3,6 +3,10 @@ import { BasicObject } from "./base"
 export interface BasicVO extends BasicObject {
   id?: number
 }
+export interface LoginVO extends BasicObject {
+  account: string,
+  token: string
+} 
 export interface ArticleVO extends BasicVO {
   title: string,
   summary: string,
