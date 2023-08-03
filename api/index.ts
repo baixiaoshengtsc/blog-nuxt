3f9d481deab8da6487178631f2a013f1ac6ae5c9
baixiaoshengtsc <485434766@qq.com>
2023-08-03 17:37:49 +0800

    🐞 fix(useFetch钩子): 修改watch配置

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,7 @@
  * @Author: baixiaoshengtsc 485434766@qq.com
  * @Date: 2023-07-15 18:18:34
  * @LastEditors: baixiaoshengtsc 485434766@qq.com
- * @LastEditTime: 2023-07-15 22:07:18
+ * @LastEditTime: 2023-07-21 17:02:26
  * @FilePath: \blog-nuxt\api\index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -17,6 +17,7 @@ const fetchConfig:UseFetchOptions<Record<string, any>> = {
     "Content-Type": "application/json"
   },
   server: false,
+  watch: false,
   credentials: 'include',
   /** 响应数据的钩子 */
   onResponse(val) {
