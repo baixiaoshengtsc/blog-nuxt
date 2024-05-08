e75f66246df8201c6ae986d9eede24a11c080ed8
baixiaoshengtsc <485434766@qq.com>
2024-05-08 10:54:50 +0800

    🐞 fix(流式生成标点符号修复):

--- store/index.ts
+++ store/index.ts
@@ -2,7 +2,7 @@
  * @Author: baixiaoshengtsc 485434766@qq.com
  * @Date: 2024-02-19 00:48:01
  * @LastEditors: baixiaoshengtsc 485434766@qq.com
- * @LastEditTime: 2024-03-01 16:12:54
+ * @LastEditTime: 2024-05-08 10:54:12
  * @FilePath: \blog-nuxt\store\index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -262,7 +262,7 @@ openai响应错误，可能存在的问题如下：
           _this.chatList[chatIndex].latestDate = _this.dataList[dataIndex].data[userIndex].time
           return
         }
-        let data = text.replace(/[^\x20-\x7E\u4E00-\u9FFF]+/g, "").split('data:')
+        let data = text.split('data:')
         data.forEach(item => {
           if (item.indexOf('DONE') !== -1) {
             return
--- store/updateList.js
+++ store/updateList.js
@@ -2,11 +2,15 @@
  * @Author: baixiaoshengtsc 485434766@qq.com
  * @Date: 2024-02-29 23:37:56
  * @LastEditors: baixiaoshengtsc 485434766@qq.com
- * @LastEditTime: 2024-03-22 12:02:06
+ * @LastEditTime: 2024-05-08 10:53:32
  * @FilePath: \blog-nuxt\store\updateList.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 export const updateList = [
+  {
+    time: '2024-5-8 v1.1',
+    msg: `修复了不知道为什么我写了一个正则把所有中文标点符号都去除的bug，现在支持正常显示标点符号了！`
+  },
   {
     time: '2024-3-22 v1.1',
     msg: `由于信用卡发卡行结算故障，故4.0相关api暂时无法使用，请自行切换为3.5，预计3.22日晚间修复`
