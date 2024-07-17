cd702d7f6e033a5aeca715cadd6ab866a0f1df0e
baixiaoshengtsc <485434766@qq.com>
2024-07-17 10:07:44 +0800

    🐞 fix(chat回复逻辑试图修复):

--- store/index.ts
+++ store/index.ts
@@ -2,7 +2,7 @@
  * @Author: baixiaoshengtsc 485434766@qq.com
  * @Date: 2024-02-19 00:48:01
  * @LastEditors: baixiaoshengtsc 485434766@qq.com
- * @LastEditTime: 2024-05-15 11:24:29
+ * @LastEditTime: 2024-07-17 10:05:25
  * @FilePath: \blog-nuxt\store\index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -268,12 +268,17 @@ openai响应错误，可能存在的问题如下：
             return
           } else {
             if (item) {
-              const format = JSON.parse(item)
-              _this.dataList[dataIndex].data[userIndex].success = true
-              _this.dataList[dataIndex].data[assistantIndex].success = true
-              if (format.choices[0].delta?.content) {
-                _this.dataList[dataIndex].data[assistantIndex].content += format.choices[0].delta?.content
+              try {
+                const format = JSON.parse(item)
+                _this.dataList[dataIndex].data[userIndex].success = true
+                _this.dataList[dataIndex].data[assistantIndex].success = true
+                if (format.choices[0].delta?.content) {
+                  _this.dataList[dataIndex].data[assistantIndex].content += format.choices[0].delta?.content
+                }
+              } catch (error) {
+
               }
+
             }
           }
         })
