3b501bfc4377d9a06625ca33edf23fc1d352250b
baixiaoshengtsc <485434766@qq.com>
2024-07-17 15:51:13 +0800

    🐞 fix(试图修改截断的消息体问题):

--- store/index.ts
+++ store/index.ts
@@ -2,7 +2,7 @@
  * @Author: baixiaoshengtsc 485434766@qq.com
  * @Date: 2024-02-19 00:48:01
  * @LastEditors: baixiaoshengtsc 485434766@qq.com
- * @LastEditTime: 2024-05-15 11:24:29
+ * @LastEditTime: 2024-07-17 15:49:53
  * @FilePath: \blog-nuxt\store\index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -225,6 +225,7 @@ export const useChatList = defineStore('chatList', {
       const reader = new TextDecoder('utf-8')
       const pipe = res.body?.getReader()
       const _this = this
+      let cache = ''
       pipe?.read().then(function util({ done, value }): any {
         if (done) {
           console.log('Stream finished');
@@ -234,7 +235,7 @@ export const useChatList = defineStore('chatList', {
           _this.chatList[chatIndex].latestDate = _this.dataList[dataIndex].data[userIndex].time
           return;
         }
-        const text = reader.decode(value);
+        const text = cache + reader.decode(value);
         if (text.indexOf('openai响应错误') !== -1) {
           _this.dataList[dataIndex].data[userIndex].success = false
           _this.dataList[dataIndex].data[assistantIndex].success = false
@@ -268,11 +269,15 @@ openai响应错误，可能存在的问题如下：
             return
           } else {
             if (item) {
-              const format = JSON.parse(item)
-              _this.dataList[dataIndex].data[userIndex].success = true
-              _this.dataList[dataIndex].data[assistantIndex].success = true
-              if (format.choices[0].delta?.content) {
-                _this.dataList[dataIndex].data[assistantIndex].content += format.choices[0].delta?.content
+              try {
+                const format = JSON.parse(item)
+                _this.dataList[dataIndex].data[userIndex].success = true
+                _this.dataList[dataIndex].data[assistantIndex].success = true
+                if (format.choices[0].delta?.content) {
+                  _this.dataList[dataIndex].data[assistantIndex].content += format.choices[0].delta?.content
+                }
+              } catch (error) {
+                cache = item
               }
             }
           }
